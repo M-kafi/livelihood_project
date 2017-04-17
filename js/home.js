21a87d8e8beccdc6dd86dac10a57c279d643a6ee
M.Kafi <mohamadkafi1985@gmail.com>
2017-04-17 15:14:47 -0400

    adding the logo / 17-04 - 2017

---
+++ js/home.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    
+  
+   
+    
+  $("secTwo").scroll( function(){
+      
+        alert("Hello! I am an alert box!!");
+  } )    
+    
+$("#burgerButton").click( function(){  
+    
+    
+  //  $("#burgerButton").animate({     right:'200px'   });
+    
+    
+    alert("Hello! I am an alert box!!");
+
+
+
+
+});
+     
+    
+    
+    
+    
+});
