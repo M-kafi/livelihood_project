003b7c6a3373caf132ffdbadcfef4de16e112578
M.Kafi <mohamadkafi1985@gmail.com>
2017-04-25 15:03:27 -0400

    working on the projects section the jqurry done - 25-04-2017

--- js/home.js
+++ js/home.js
@@ -25,22 +25,80 @@ $(document).ready(function(){
      
  } );
 
- //--end of th function to show and hide the navigation bar --
+ //--end of the function to show and hide the navigation bar --
 
-
-        
-   // $("#one").hide();
- //  $("#two").hide();
-   //$("#three").hide();
     
     
     $("#one").animate({width:'250px', paddingleft:'20px', opacity: '1', right: '0px' },1000);
     $("#two").animate({width:'250px', paddingleft:'20px', opacity: '1', right: '0px'}, 2000 );
     $("#three").animate({width:'250px', paddingleft:'20px', opacity: '1', right: '0px'},3000 );
     
+    
+    
+    
+    //--------------projects section------------
+    
+    
+    
+    //---- trigger the event when the mouse over the element -----
+    $(".part").mouseover( function(){
+        
+        
+        
+        var el = this ;
+        
+        //--- only start the event if the mouse stays for specific time over the element ---
+         start = setTimeout(function(){ 
+         $( el).nextAll().animate({left: '+=2.5%'});
+        $( el).prevAll().animate({left: '-=2.5%'});
+       $( el).animate({width: '+=5%',
+                         left: '-=2.5%',
+                        zIndex:'9'
+                        
+                        });
+                         
+        //--- after the event is done change the value to 1                 
+               done = 1;          
+                         
+                         
+                         
+                         }, 500);
+      
+        
+      
+    } );
+  
+    
+    
+    //----- trigger the event only if the mouse leaves the element and the done is 1
+     $(".part").mouseout( function(){
+         
+         //--- stop the time out event to not trigger the first event and not doing any changes ---
+          clearTimeout(start);
+        
+         
+         
+         var el  = this;
+         
+         //---- if the previous event occurred then trigger the event to set the properties to it's initial values ----
+         if ( done == 1 )
+             {
+       $( el).nextAll().animate({left: '-=2.5%'});
+          $( el).prevAll().animate({left: '+=2.5%'});
+         $( el).animate({width: '-=5%',
+                          left: '+=2.5%',
+                            zIndex:'5'
+                          
+                          });
+                 done = 0;
+             }
+                          
+        
+    } );
+    
 });
      
     
-    
+ //-------END of project section ------   
     
     
